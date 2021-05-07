d63b6e9350d28969af512975b938aab22d21a099
xiaoyu <yuriyshea@163.com>
2021-05-07 13:10:45 +0800

    导航菜单

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.less';
+import React from 'react'
+import { Layout } from 'antd';
+import { Content, Footer } from 'antd/lib/layout/layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="App">
+      左侧导航菜单
+      <Layout>
+        Header
+        <Content className='App-content'>
+          Hello
+        </Content>
+        <Footer className='App-footer'>
+          <a href="https://github.com/">React Demo</a>
+        </Footer>
+      </Layout>
+    </Layout>
   );
 }
 
